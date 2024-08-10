e9bfc495257927f2e82c62948f0e8d9c4ea96f36
Vu Hoang <hoangvh123456@gmail.com>
2024-08-10 18:14:41 +0900

    final commit

--- starter/backend/src/lambda/auth/auth0Authorizer.mjs
+++ starter/backend/src/lambda/auth/auth0Authorizer.mjs
@@ -4,27 +4,7 @@ import { createLogger } from '../../utils/logger.mjs'
 
 const logger = createLogger('auth')
 
-const jwksUrl = 'https://test-endpoint.auth0.com/.well-known/jwks.json'
-
-const certificate = `-----BEGIN CERTIFICATE-----
-MIIDHTCCAgWgAwIBAgIJL42r+jPLONF6MA0GCSqGSIb3DQEBCwUAMCwxKjAoBgNV
-BAMTIWRldi04M3RwbWY0aGlqczhvZmZ1LnVzLmF1dGgwLmNvbTAeFw0yNDA3MjUx
-NTUwMThaFw0zODA0MDMxNTUwMThaMCwxKjAoBgNVBAMTIWRldi04M3RwbWY0aGlq
-czhvZmZ1LnVzLmF1dGgwLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
-ggEBAN4mFxaod04x+Ncgn8nt7VwWQagXC6Mj+BeYPMcWiNzRQAW7MK1KZ1QZpdqM
-nN2sf+8Pwg34ogf6tZHy60WHZu2EGwOYp4ZgxgNv2IvHdxAi7tHlkGtAzEQBAZgV
-VpbO5nJY+ViAeqCAIrMn5fL1nGrX1iBwl/H9u9B2pAzRC6cO4HXmL9hgKs0twKCs
-zPvfpfA/2fm25wzz6QOqBr+7sXribP0P+npUhr0QcF5p1KSJWop421s0oVxcKtUN
-afcrGa7JoqPPMWjphaoOlF4m5hoDEAuxKxH/zhAaIBozLEmIl6h12+BeOEaBHGos
-q+JvlW3xTlfe2dckxksJfr3wEYsCAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAd
-BgNVHQ4EFgQUsjZFb8FCNf6dCwCcRbJFwKxvsIIwDgYDVR0PAQH/BAQDAgKEMA0G
-CSqGSIb3DQEBCwUAA4IBAQA2kGTMKDngqZ7jd0kz9NHQTIMCPsVu0MNxfiUirhHd
-sq+RASPZZASb6s9sLbDJTGiBd4ke31ljj8/KVyPQxEub7dR20U0Z4BYROdVzhDW6
-HgEXSykvOGHYpg9qElSaa0vNkkitkAzkLdXx+8uIWH56nVQAiwCmlVH0OSm6YPae
-79ak8htUQmnPCcSglJIq5Fvj5HgaYHcn3zVuB8Rwkx9AiWzy+MkQ5UZYzyH0X2ve
-rdWUOapxxRIVYKXy0DtuWoXIYPhLhyrx+Jo7trfHlSQPcXHmOGfhJnZW8Kh0+RKq
-oCRvWHuGMJq38ygMf2tkKpA/Ilu7HC3aSamx1NcUeGWI
------END CERTIFICATE-----`
+const jwksUrl = 'https://dev-wvel68sr6yhpq7qj.us.auth0.com/.well-known/jwks.json'
 
 export async function handler(event) {
   try {
@@ -63,17 +43,23 @@ export async function handler(event) {
 }
 
 async function verifyToken(authHeader) {
-  if (!authHeader) throw new Error('No authorization header')
-
-  if (!authHeader.toLowerCase().startsWith('bearer ')) {
-    throw new Error('Invalid authorization header')
-  }
-
   const token = getToken(authHeader)
   const jwt = jsonwebtoken.decode(token, { complete: true })
 
-  // TODO: Implement token verification
-  return jsonwebtoken.verify(token, certificate, { algorithms: ['RS256'] });
+  const res = await Axios.get(jwksUrl);
+  const keys = res.data.keys;
+  const signKeys = keys.find(key => key.kid === jwt.header.kid);
+
+
+  if (!signKeys) throw new Error("Invalid signature Keys");
+  const key = signKeys.x5c[0];
+  const certificate = `-----BEGIN CERTIFICATE-----\n${key}\n-----END CERTIFICATE-----\n`;
+
+  const verifyToken = jsonwebtoken.verify(token, certificate, { algorithms: ['RS256'] });
+
+  logger.info('Token', verifyToken);
+
+  return verifyToken;
 }
 
 function getToken(authHeader) {
---
+++ starter/client/src/config.js
@@ -0,0 +1,4 @@
+export const AUTH0_DOMAIN_NAME='dev-wvel68sr6yhpq7qj.us.auth0.com'
+export const AUTH0_CLIENT_ID='HkD2VyxHZ0kcQe5jkMCUKiN7uPxqJHpd'
+export const AUTH0_AUDIENCE=`https://dev-wvel68sr6yhpq7qj.us.auth0.com/api/v2/`
+export const API_DESTINATION=`https://xta4dmnqdd.execute-api.us-east-1.amazonaws.com/dev`
